9606cbaa56b776a36dd1d581aafcba86c106037e
jgraveme <jgraveme@smail.uni-koeln.de>
2016-09-23 14:28:22 +0100

    Update run_command.py

--- run_command.py
+++ run_command.py
@@ -37,23 +37,4 @@ def check_programs(*arg):
     for program in arg:
         which(program)
 
-######
-@contextlib.contextmanager
-def cd(newdir, cleanup=lambda: True):
-    prevdir = os.getcwd()
-    os.chdir(os.path.expanduser(newdir))
-    try:
-        yield
-    finally:
-        os.chdir(prevdir)
-        cleanup()
 
-
-@contextlib.contextmanager
-def tempdir():
-    dirpath = tmp.mkdtemp()
-
-    def cleanup():
-        shutil.rmtree(dirpath)
-    with cd(dirpath, cleanup):
-        yield dirpath

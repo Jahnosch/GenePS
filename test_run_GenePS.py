b79dc6daace9d4cdf9e87720e01a28413e1a962e
jgraveme <jgraveme@smail.uni-koeln.de>
2016-09-23 16:39:13 +0100

    testing run_GenePS

--- test_run_GenePS.py
+++ test_run_GenePS.py
@@ -5,4 +5,7 @@ from run_GenePS import *
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 test_data = os.path.join(script_path, "group1.makeGenePS")
-test_db = os.path.join(script_path, "")
+test_db = os.path.join(script_path, "c_elegans.PRJNA13758.WS254.genomic")
+
+print(test_db)
+print(script_path)

883b93b5545f68c96751ab75460d043c528efd38
jgraveme <jgraveme@smail.uni-koeln.de>
2016-09-27 14:53:34 +0100

    with hmmer

--- test_make_GenePS.py
+++ test_make_GenePS.py
@@ -151,7 +151,6 @@ class TestScoreObject(unittest.TestCase):
             scoring_obj = ScoreObject(self.fa_hash, self.left_taxa, file_name, tmp_dir)
             self.assertTrue(scoring_obj.name == file_name + ".hmmGenePS")
 
-    #@unittest.skipUnless(os.getcwd().split("/")[-1] == "GenePS", "test_phmm_path_not_none requires HMMER")
     def test_phmm_path_not_none(self):
         with tempdir() as tmp_dir:
             scoring_obj = ScoreObject(self.fa_hash, self.left_taxa, file_name, tmp_dir)
@@ -159,7 +158,6 @@ class TestScoreObject(unittest.TestCase):
             scoring_obj.compute_full_phmm()
             self.assertTrue(scoring_obj.hmm_path)
 
-    @unittest.skipUnless(os.getcwd().split("/")[-1] == "GenePS", "test_correct_score_list requires HMMER")
     def test_correct_score_list(self):
         with tempdir() as tmp_dir:
             scoring_obj = ScoreObject(self.fa_hash, self.left_taxa, file_name, tmp_dir)
@@ -173,7 +171,6 @@ class TestScoreObject(unittest.TestCase):
             control = ">MINCO1.Minc01141" + "\n" + "".join(self.fa_hash[">MINCO1.Minc01141"])
             self.assertTrue(fasta_string == control)
 
-    @unittest.skipUnless(os.getcwd().split("/")[-1] == "GenePS", "test_get_consensus requires HMMER")
     def test_get_consensus(self):
         with tempdir() as tmp_dir:
             cons_hmm = os.path.join(tmp_dir, file_name + ".chmm")
